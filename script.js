66069bff7007a0907ee07a3ce9b4314112394dfd
Mimidz09 <mebrouka.rbh85@gmail.com>
2022-11-03 14:58:58 +0100

    script fin

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const apikey = "df7c20338910b9d51c62838c095ab7ec";
 const latLiege = 50.6333;
 const lonLiege = 5.56667;
 const langage = "fr";
-const counter = 7;
+const counter = 40;
 
 // https://api.openweathermap.org/data/2.5/forecast?lat=50.6333&lon=5.56667&appid=df7c20338910b9d51c62838c095ab7ec&lang=fr&cnt=7&units=metric
 
@@ -19,12 +19,12 @@ function fetchingDatas() {
           true
         );
         templateElement.getElementById("date").textContent = call.dt_txt;
-        templateElement.getElementById("maxTemp").textContent = Math.round(
+        templateElement.getElementById("maxTemp").textContent = `${Math.round(
           call.main.temp_max
-        );
-        templateElement.getElementById("minTemp").textContent = Math.round(
+        )} °C`;
+        templateElement.getElementById("minTemp").textContent = `${Math.round(
           call.main.temp_min
-        );
+        )} °C`;
         templateElement.getElementById("windSpeed").textContent = Math.round(
           call.wind.speed
         );
